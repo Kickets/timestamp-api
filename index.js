2171fcfb2be4d006af5ec6c474e0116f2fe8bb9e
Jake Mitchell <kickets1691@gmail.com>
2017-05-24 05:53:08 +0000

    bugfix

--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = function(app){
-  app.route('/').get(function(req,res){
-      res.sendFile(process.cwd() + '/index.html');
-    });
-};
--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var routes = require("./index.js");
-var api = require("./timestamp.js");
+var routes = require("./app/routes/index.js");
+var api = require("./app/api/timestamp.js");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', express.static(process.cwd() + '/'));
+app.use('/public', express.static(process.cwd() + '/public'));
 var port = process.env.PORT || 8080;
 routes(app);
 api(app);
 app.listen(port, function() {
-    console.log("server.js listening on port " + port + "...");
+    console.log("Node.js listening on port " + port + "...");
 });
